da8fc0f8ad196fa50f26e407281e03a9de254b54
abeln <544125@unizar.es>
2020-05-09 13:14:06 +0200

    better, using poda

--- abel/P2/test.py
+++ abel/P2/test.py
@@ -1,21 +1,38 @@
 from queue import PriorityQueue
 
 
-def dist(x, y, xp, yp):
-    return abs(xp - x) + abs(yp - y)
+def dist(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
-def cost(chosed):
+def _cost(secuence):
     cost = 0
 
-    xp, yp = x0, y0
-    for x, y in chosed:
-        cost += dist(x, y, xp, yp)
-        xp, yp = x, y
+    prev = initial
+    for c in secuence:
+        cost += dist(c, prev)
+        prev = c
 
-    cost += dist(xp, yp, x0, y0)
+    cost += dist(prev, initial)
 
     return cost
+
+
+def cost(chosed, available):
+    if len(available) == 0:
+        cost = _cost(chosed)
+        return cost, cost
+
+    last = chosed[-1] if len(chosed)>0 else initial
+    far = max(available, key=lambda p: dist(p, last))
+    mincost = _cost(chosed + [far])
+
+    maxcost = mincost
+
+    for c in [c for c in available if c != far]:
+        maxcost += _cost([c])
+
+    return mincost, maxcost
 
 
 with open('prueba.txt') as input:
@@ -23,7 +40,7 @@ with open('prueba.txt') as input:
     with open('sol_prueba.txt') as output:
         for n in range(N):
             A, B = [int(x) for x in next(input).split()]
-            x0, y0 = [int(x) for x in next(input).split()]
+            initial = [int(x) for x in next(input).split()]
             M = int(next(input))
             SOL, _ = [x for x in next(output).split()]
             SOL = int(SOL)
@@ -31,18 +48,22 @@ with open('prueba.txt') as input:
             mines = []
             for m in range(M):
                 mine = [int(x) for x in next(input).split()]
-                if mine not in mines and mine != (x0, y0): mines.append(mine)
+                if mine not in mines and mine != initial: mines.append(mine)
 
             best = None
             bestcost = float("inf")
 
+            firstcost, cota = cost([], mines)
+
             nodes = PriorityQueue()
-            nodes.put((0, ([], mines)))
+            nodes.put((firstcost, ([], mines)))
             while not nodes.empty():
                 thecost, (chosed, available) = nodes.get()
 
-                if thecost >= bestcost: continue
+                # poda
+                if thecost > cota: continue
 
+                # update solution if better
                 if not available and thecost < bestcost:
                     bestcost = thecost
                     best = chosed
@@ -52,10 +73,15 @@ with open('prueba.txt') as input:
                     newchosed.append(mine)
                     newavailable = available.copy()
                     newavailable.remove(mine)
-                    nodes.put((cost(newchosed), (newchosed, newavailable)))
+
+                    newcost, newcota = cost(newchosed, newavailable)
+
+                    if newcota < cota: cota = newcota
+
+                    nodes.put((newcost, (newchosed, newavailable)))
 
             if bestcost != SOL:
                 print()
             else:
                 print(".")
-            print("CORRECTO:" if bestcost != SOL else "ERROR:", f"se esperaba {SOL} y se ha obtenido {bestcost} {best}")
+            print("CORRECTO:" if bestcost == SOL else "ERROR:", f"se esperaba {SOL} y se ha obtenido {bestcost} {best}")
