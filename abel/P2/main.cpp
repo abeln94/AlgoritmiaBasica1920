97af7a95a2cf6aba1576f9d5f25943c4b452de20
abeln <544125@unizar.es>
2020-05-09 12:48:51 +0200

    Moved files to subfolder to allow P1 and P2

---
+++ abel/P2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <fstream>
+#include <functional>
+#include <thread>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
